d5e1e2f014692ee5c42ae02b0d84b3c5089a5446
Matias Vidoni <matiasrmontenegro@gmail.com>
2021-07-05 18:52:03 -0300

    Initial Commit

---
+++ Billetera/BilleteraApp.swift
@@ -0,0 +1,17 @@
+//
+//  BilleteraApp.swift
+//  Billetera
+//
+//  Created by Mati Montenegro on 05/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct BilleteraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Billetera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Billetera
+//
+//  Created by Mati Montenegro on 05/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
